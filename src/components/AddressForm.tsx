df93c060724e03d613692f48be56d716cd09f2bb
Chris Wren <chris@drwren.com>
2025-06-04 08:35:22 -0400

    WIP add a drop-down for states
    TODO: figure why opening the drop down resizes
    the entire page.

--- src/components/AddressForm.tsx
+++ src/components/AddressForm.tsx
@@ -16,6 +16,15 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 import { Input } from "@/components/ui/input"
 
 const state_codes = Object.keys(states);
@@ -23,6 +32,7 @@ const AddressSchema = z.object({
   name: z.string().min(2, {
     message: "name must be at least 2 characters.",
   }),
+  state: z.enum([state_codes[0], ...state_codes.slice(1)]),  // ew, I hate this
 });
 
 export default function AddressForm() {
@@ -30,6 +40,7 @@ export default function AddressForm() {
     resolver: zodResolver(AddressSchema),
     defaultValues: {
       name: "",
+      state: "MA",
     },
   })
 
@@ -48,6 +59,32 @@ export default function AddressForm() {
               <FormLabel>Name</FormLabel>
               <FormControl>
                 <Input placeholder="John Doe" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {/* see https://github.com/shadcn-ui/ui/issues/1253 */}
+        <FormField
+          control={form.control}
+          name="state"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>State</FormLabel>
+              <FormControl>
+                <Select onValueChange={field.onChange} value={field.value}>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a state" />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectLabel>States</SelectLabel>
+                        {Object.entries(states).map(([code, desc]) => (
+                          <SelectItem value={code}>{desc["name"]}</SelectItem>
+                        ))}
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
               </FormControl>
               <FormMessage />
             </FormItem>
